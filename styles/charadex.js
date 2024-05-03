b5f2d0c0590414d95f8489b2df95bb17c542ea01
Shadowkitty11 <166874797+Shadowkitty11@users.noreply.github.com>
2024-05-02 20:29:25 -0500

    Update charadex.js

--- styles/charadex.js
+++ styles/charadex.js
@@ -56,7 +56,7 @@ const scrubData = (sheetData) => {
 let optionSorter = (options) => {
 
     // Clean up the sheetID - in case they used a link instead
-    let scrubbedSheetId = sheetID ? sheetID.includes('/d/') ? sheetID.split('/d/')[1].split('/edit')[0] : sheetID : "1l_F95Zhyj5OPQ0zs-54pqacO6bVDiH4rlh16VhPNFUc";
+    let scrubbedSheetId = sheetID ? sheetID.includes('/d/') ? sheetID.split('/d/')[1].split('/edit')[0] : sheetID : "1aCKu9IconNCu7ZlezYrje6RRPHtEOBtdk0FzLlGMamo";
 
     // Call all options, make defaults of our own
     let userOptions = options;
